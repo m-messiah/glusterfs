6f71dc8e417d56cfc916b118478bb34253ba9a3e
Messiah <m.muzafarov@gmail.com>
2013-06-18 16:25:54 +0600

    renew again

---
+++ installation.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+./autogen.sh
+./configure --prefix=/usr \
+--build=i686-pc-linux-gnu \
+--host=i686-pc-linux-gnu \
+--mandir=/usr/share/man \
+--infodir=/usr/share/info \
+--datadir=/usr/share \
+--sysconfdir=/etc \
+--localstatedir=/var/lib \
+--libdir=/usr/lib \
+--disable-dependency-tracking \
+--enable-fuse-client \
+--disable-ibverbs \
+--disable-static \
+--enable-georeplication \
+--disable-bdb \
+--docdir=/usr/share/doc/glusterfs\
+CFLAGS="-O3 -pipe"\
+CXXFLAGS="${CFLAGS}"
+make
+make install
