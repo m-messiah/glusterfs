4781dc312df4ba613076df4ced6047a49baa8e4c
root <root@front-test.sd.kontur.ru>
2014-03-07 09:37:14 +0400

    remove trash

--- debug/plot.py
+++ debug/plot.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 import numpy
 from matplotlib import pyplot as plt
-datatype=[('index', numpy.int32), ('floati', numpy.float32)]
 x=map(float, open("1.txt",'r').readlines())
 plt.plot(range(len(x)), x, 'r')
 plt.grid(True)
