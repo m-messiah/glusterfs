71bd9e77c6f1378064749a2aba36d0fa8beafd16
root <root@front-test.sd.kontur.ru>
2014-03-11 11:27:51 +0400

    fix plotter

--- debug/plot.py
+++ debug/plot.py
@@ -3,11 +3,10 @@ import matplotlib
 matplotlib.use('svg')
 import matplotlib.pyplot as plt
 j=0
-#for i in ["1k", "4k", "10k", "100k", "1M", "10M", "40M", "80M", "100M", "1G"]:
-for i in ["1G"]:
+for i in ["1k", "4k", "10k", "100k", "1M", "10M", "40M", "80M", "100M", "1G"]:
 	try:
 		plt.figure(j)
-		x=map(float, open("./result/LRU/1*10000*" + i + ".txt", 'r').readlines())
+		x=map(float, open("./result/MRU/1*10000*" + i + ".txt", 'r').readlines())
 		plt.plot(range(len(x)), x, 'r')
 		plt.grid(True)
 		plt.savefig("./result/LRU/1*10000*" + i + ".svg")
