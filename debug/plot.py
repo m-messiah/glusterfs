8e6241daaf092518350f08e74c715cc3f56e3127
root <root@front-test.sd.kontur.ru>
2014-03-12 15:46:29 +0400

    some more spped

--- debug/plot.py
+++ debug/plot.py
@@ -3,14 +3,15 @@ import matplotlib
 matplotlib.use('svg')
 import matplotlib.pyplot as plt
 j=0
-for i in ["1k", "4k", "10k", "100k", "1M", "10M", "40M", "80M", "100M", "1G"]:
-	try:
-		plt.figure(j)
-		x=map(float, open("./result/MRU/1*10000*" + i + ".txt", 'r').readlines())
-		plt.plot(range(len(x)), x, 'r')
-		plt.grid(True)
-		plt.savefig("./result/LRU/1*10000*" + i + ".svg")
-		print("Saved " + i)
-		j+=1
-	except:
-		pass
+for i in ["1k", "4k", "10k", "100k", "1M", "10M", "40M", "80M", "100M", "1024M"]:
+	for t in ["1000", "10000"]:
+		try:
+			plt.figure(j)
+			x=map(float, open("./result/MRU/1*" + t + "*" + i + ".txt", 'r').readlines())
+			plt.plot(range(len(x)), x, 'r')
+			plt.grid(True)
+			plt.savefig("./result/LRU/1*" + t + "*" + i + ".svg")
+			print("Saved " + i)
+			j+=1
+		except:
+			pass
--- debug/test.sh
+++ debug/test.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-ALGO="LRU"
+ALGO="MRU"
 mkdir -p ./result/$ALGO
 
 THREADS=1
-TRIES=10000
-FILES=$(( TRIES / 50))
+TRIES=1000
+FILES=$(( TRIES / 40))
 
-for SIZE in 1k 10k 100k 1M 10M 40M 80M 100M 1G
+for SIZE in 1k 10k 100k 1M
 do
 	for i in `seq 1 $FILES`
 	do
@@ -19,5 +19,20 @@ do
 		starttime=`date +%s.%N`
 		cat "/d/$(( RANDOM % FILES + 1 )).test" > testfile.test
 		echo "$(date +%s.%N) - $starttime" | bc >> result/${ALGO}/$THREADS*$TRIES*${SIZE}.txt
+	done;
+done
+
+for SIZE in 10 40 80 100 1024
+do
+	for i in `seq 1 $FILES`
+	do
+		dd if=/dev/urandom of=/d/$i.test bs=1M count=$SIZE 2> /dev/null
+	done
+	
+	for j in `seq 1 $TRIES`
+	do
+		starttime=`date +%s.%N`
+		cat "/d/$(( RANDOM % FILES + 1 )).test" > testfile.test
+		echo "$(date +%s.%N) - $starttime" | bc >> result/${ALGO}/$THREADS*$TRIES*${SIZE}M.txt
 	done;
 done
