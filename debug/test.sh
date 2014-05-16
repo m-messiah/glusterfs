81a264885a42893f8418b4e64fe5e7fb72be1d19
M_messiah <m.muzafarov@gmail.com>
2014-05-16 09:19:39 +0600

    Write results

--- debug/test.sh
+++ debug/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 algos=( LRU MRU FIFO LFU )
-CLIENTS=4
+CLIENT=4
 
 function testReadK() {
     CLIENT=$1
@@ -28,6 +28,32 @@ function testReadK() {
             seq $threads | parallel -n0 cat "/d${CLIENT}_{#}/$(( count % FILES + 1 )).test" > testfile.test || exit 1
             echo "$(date +%s.%N) - $starttime" | bc >> result/$ALGO/$TYPE$threads*$TRIES*${SIZE}k.txt
         done
+    done
+}
+
+function testWriteK() {
+    CLIENT=$1
+    SIZE=$2
+    ALGO=$3
+    TYPE=$4
+    rm -rf /d${CLIENT}_1/*
+    for i in `seq 1 $FILES`
+    do
+        dd if=/dev/urandom of=/d${CLIENT}_1/$i.test bs=${SIZE}k count=1 2> /dev/null || exit 1
+    done
+    
+    for j in $(seq 1 $TRIES)
+    do
+        starttime=$(date +%s.%N)
+        if [[ $TYPE == rand ]]
+            then
+                count=$RANDOM
+                parallel ::: "dd if=/dev/urandom of=/d${CLIENT}_1/${RANDOM}.test bs=${SIZE}k count=1 2> /dev/null" "cat \"/d${CLIENT}_2/$(( count % FILES + 1 )).test\" > testfile.test"
+            else
+                count=$j
+                parallel ::: "dd if=/dev/urandom of=/d${CLIENT}_1/${count}.test bs=${SIZE}k count=1 2> /dev/null" "cat \"/d${CLIENT}_2/$(( count % FILES + 1 )).test\" > testfile.test"
+        fi
+        echo "$(date +%s.%N) - $starttime" | bc >> result/$ALGO/$TYPE*write*$TRIES*${SIZE}k.txt
     done
 }
 
@@ -59,8 +85,33 @@ function testReadM() {
     done
 }
 
-for CLIENT in $(seq 4 $CLIENTS)
-do
+function testWriteM() {
+    CLIENT=$1
+    SIZE=$2
+    ALGO=$3
+    TYPE=$4
+    rm -rf /d${CLIENT}_1/*
+    for i in `seq 1 $FILES`
+    do
+        dd if=/dev/urandom of=/d${CLIENT}_1/$i.test bs=1M count=$SIZE 2> /dev/null || exit 1
+    done
+    
+    for j in $(seq 1 $TRIES)
+    do
+        starttime=$(date +%s.%N)
+        if [[ $TYPE == rand ]]
+            then
+                count=$RANDOM
+                parallel ::: "dd if=/dev/urandom of=/d${CLIENT}_1/${RANDOM}.test bs=1M count=$SIZE 2> /dev/null" "cat \"/d${CLIENT}_2/$(( count % FILES + 1 )).test\" > testfile.test"
+            else
+                count=$j
+                parallel ::: "dd if=/dev/urandom of=/d${CLIENT}_1/${count}.test bs=1M count=$SIZE 2> /dev/null" "cat \"/d${CLIENT}_2/$(( count % FILES + 1 )).test\" > testfile.test"
+        fi
+        echo "$(date +%s.%N) - $starttime" | bc >> result/$ALGO/$TYPE*write*$TRIES*${SIZE}M.txt
+    done
+}
+#for CLIENT in $(seq 4 $CLIENTS)
+#do
     ALGO=${algos[$((CLIENT - 1))]}
     #rm -rf ./result/$ALGO
     #mkdir -p ./result/$ALGO
@@ -68,15 +119,19 @@ do
     TRIES=1000
     FILES=$(( TRIES / 20))
     
-    #for SIZE in 10 100 200
-    #do
-    #    testReadK $CLIENT $SIZE $ALGO rand
-    #    testReadK $CLIENT $SIZE $ALGO seq
-    #done
-    
-    for SIZE in 1
+    for SIZE in 10 100 200
     do
-        testReadM $CLIENT $SIZE $ALGO rand
-        testReadM $CLIENT $SIZE $ALGO seq
+        #testReadK $CLIENT $SIZE $ALGO rand
+        #testReadK $CLIENT $SIZE $ALGO seq
+        testWriteK $CLIENT $SIZE $ALGO rand
+        testWriteK $CLIENT $SIZE $ALGO seq
     done
-done
+    
+    for SIZE in 1 10
+    do
+        #testReadM $CLIENT $SIZE $ALGO rand
+        #testReadM $CLIENT $SIZE $ALGO seq
+        testWriteM $CLIENT $SIZE $ALGO rand
+        testWriteM $CLIENT $SIZE $ALGO seq
+    done
+#done
